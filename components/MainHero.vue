2bd0a01e1f8d0fd331935a031cf3be16d314817a
Chrisimir <chrislund97@gmail.com>
2020-11-05 21:22:24 +0100

    added initial components

---
+++ components/MainHero.vue
@@ -0,0 +1,32 @@
+<template>
+    <div id="mainHero">
+        <div class="container">
+            <div class="row align-items-center justify-content-between">
+                <b-col md="6">
+                    <div>
+                        <div class="mt-4 mt-sm-0">
+                            <h1>PDGR</h1>
+                            <h2>Ejemplo</h2>
+                            <p>Texto de ejemplo</p>
+                            <p>
+                                <b-button class="d-md-none" block squared to="/donar">Donar</b-button>
+                                <b-button class="d-none d-md-inline" squared size="lg" to="/donar">Donar</b-button>
+                                <b-button class="d-md-none" block squared to="/solicitar">Solicitar</b-button>
+                                <b-button class="d-none d-md-inline" squared size="lg" to="/solicitar">Solicitar</b-button>
+                            </p>
+                        </div>
+                    </div>
+                </b-col>
+                <b-col md="4" class="d-none d-md-block" style="overflow:hidden;">
+                    <div class="d-none d-md-block image"><img src="~/assets/images/holder_image.jpg" alt="Holder image" /></div>
+                </b-col>
+            </div>
+        </div>
+    </div>
+</template>
+
+<style scoped>
+.image {
+    max-height: 50vh;
+}
+</style>
---
+++ components/Navbar.vue
@@ -0,0 +1,14 @@
+<template>
+  <b-navbar toggleable="lg" type="dark" variant="info">
+    <b-navbar-brand href="#">LOGO</b-navbar-brand>
+
+    <b-collapse id="nav-collapse" is-nav>
+      
+      <b-navbar-nav>
+        <b-nav-item href="#">Solicitar</b-nav-item>
+        <b-nav-item href="#">Donar</b-nav-item>
+      </b-navbar-nav>
+
+    </b-collapse>
+  </b-navbar>
+</template>
