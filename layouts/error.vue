27c34fd844ee4b1327faec542169170e1f783f93
Chrisimir <chrislund97@gmail.com>
2020-12-02 21:50:55 +0100

    errror page

---
+++ layouts/error.vue
@@ -0,0 +1,42 @@
+<template>
+  <div>
+    Error imprevisto
+  </div>
+</template>
+
+<script>
+import Navbar from "@/components/Navbar";
+import Footer from "@/components/Footer";
+
+export default {
+  components: {
+    Navbar,
+    Footer,
+  }
+}
+</script>
+
+<style>
+html {
+  font-family:
+    'Source Sans Pro',
+    -apple-system,
+    BlinkMacSystemFont,
+    'Segoe UI',
+    Roboto,
+    'Helvetica Neue',
+    Arial,
+    sans-serif;
+  font-size: 16px;
+  word-spacing: 1px;
+  -ms-text-size-adjust: 100%;
+  -webkit-text-size-adjust: 100%;
+  -moz-osx-font-smoothing: grayscale;
+  -webkit-font-smoothing: antialiased;
+  box-sizing: border-box;
+}
+
+#main-content {
+  min-height: 100vh;
+}
+</style>
